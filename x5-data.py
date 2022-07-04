bf8276728979546b51f9fd7b089330f6d29c74aa
mehanat <mehanat1996@gmail.com>
2022-07-04 14:15:03 +0400

    data prep

--- x5-data.py
+++ x5-data.py
@@ -6,12 +6,12 @@ import os
 import pendulum
 
 
-data_path = '/opt/x5'
+data_path = '/data'
 archive_name = 'retailhero-uplift.zip'
 
 dag = DAG(
     dag_id="x5-data-preparation",
-    schedule_interval="10 * * * * *",
+    schedule_interval="*/10 * * * *",
     start_date=pendulum.datetime(2022, 1, 1, tz="UTC"),
 )
 
@@ -22,6 +22,24 @@ def download_zip_archive():
 
 def unzip_data_archive():
     os.system(f'unzip ./{archive_name}')
+
+def join_data():
+    df_clients = pd.read_csv(f'{data_path}/clients.csv', index_col='client_id')
+    df_train = pd.read_csv(f'{data_path}/uplift_train.csv', index_col='client_id')
+    df_test = pd.read_csv(f'{data_path}/uplift_test.csv', index_col='client_id')
+
+    df_clients['first_issue_unixtime'] = pd.to_datetime(df_clients['first_issue_date']).astype(int)/10**9
+    df_clients['first_redeem_unixtime'] = pd.to_datetime(df_clients['first_redeem_date']).astype(int)/10**9
+    df_features = pd.DataFrame({
+        'gender_M': (df_clients['gender'] == 'M').astype(int),
+        'gender_F': (df_clients['gender'] == 'F').astype(int),
+        'gender_U': (df_clients['gender'] == 'U').astype(int),
+        'age': df_clients['age'],
+        'first_issue_time': df_clients['first_issue_unixtime'],
+        'first_redeem_time': df_clients['first_redeem_unixtime'],
+        'issue_redeem_delay': df_clients['first_redeem_unixtime'] - df_clients['first_issue_unixtime'],
+    }).fillna(0)
+    df_features.to_csv(f'{data_path}/df_features.csv')
 
 download_zip = PythonOperator(
     task_id='download_zip',
@@ -37,5 +55,12 @@ unzip_archive = PythonOperator(
     dag=dag,
 )
 
-download_zip >> unzip_archive
+data_peprocessing = PythonOperator(
+    task_id='data_peprocessing',
+    python_callable = join_data,
+    op_kwargs = {},
+    dag=dag,
+)
+
+download_zip >> unzip_archive >> data_peprocessing
 
