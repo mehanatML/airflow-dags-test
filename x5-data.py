dc1689eb652b9d4ac70dd2188c08510c7d4affb7
mehanat <mehanat1996@gmail.com>
2022-07-04 13:59:16 +0400

    init

--- x5-data.py
+++ x5-data.py
@@ -11,13 +11,13 @@ archive_name = 'retailhero-uplift.zip'
 
 dag = DAG(
     dag_id="x5-data-preparation",
-    schedule_interval="1 * * * * *",
+    schedule_interval="10 * * * * *",
     start_date=pendulum.datetime(2022, 1, 1, tz="UTC"),
 )
 
 
 def download_zip_archive():
-    if not archive_name in os.system('ls'):
+    if not archive_name in os.listdir():
         os.system('wget https://storage.yandexcloud.net/datasouls-ods/materials/9c6913e5/retailhero-uplift.zip')
 
 download_zip = PythonOperator(
